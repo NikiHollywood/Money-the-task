7d5f280d1b8ed44b8954918d22a128b7ad40f5d9
Nikita Sizov <dog009593@yandex.ru>
2020-11-26 14:21:42 +0300

    Обновление

--- src/MoneyTransfer.java
+++
@@ -1,8 +0,0 @@
-public class MoneyTransfer {
-    public static void main(String[] args) {
-        int balance = 2000000000;
-        int  refillTransfer = 500000000;
-        balance+= refillTransfer;
-        System.out.println("Текущий баланс счета" + balance);
-    }
-}
